34a47ca02c7df51152f6f9ca7bf5aad31b46466b
tbeacodg <tbeacoding@gmail.com>
2019-12-17 05:52:26 +0100

    deputes.rb runiing

---
+++ lib/deputes.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+def getemail (urldeputy)
+	page = Nokogiri::HTML(open(urldeputy))
+	email = page.xpath('//*[@id="b1"]/ul[2]/li[1]/ul/li/a').text
+
+	nom = page.css('h1').text
+	print nom + " : "
+	puts email
+end
+
+def getallurls()
+	#url = "http://www.nosdeputes.fr/" + prenom + "-" + nom
+	doc = Nokogiri::HTML(open("https://www.nosdeputes.fr/deputes"))
+	div_classes = doc.xpath('//div[@class="list_table"]')
+	all_a = div_classes.css('a')   
+
+	allsurl = all_a.map{|element| urls = 'https://nosdeputes.fr'+element["href"]}
+
+	# puts allurl
+
+	allsurl.each { |url| puts getemail(url) }
+
+end
+
+getallurls
+#getemail
