722f669ffb6155cf94d76201819c24b3306dbf5b
tbeacodg <tbeacoding@gmail.com>
2019-12-17 05:58:10 +0100

    mairie_xstmas running

---
+++ lib/mairie_xstmas.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+def get_townhall_email(url)
+	page = Nokogiri::HTML(open(url))
+    page.css("td")[7].text
+
+end
+
+def get_all_the_emails(url)
+
+	page= Nokogiri::HTML(open(url))
+	final = Array.new
+
+	link = page.css('a[class=lientxt]')
+
+	link.each {|url|
+	hashville = Hash.new
+	hashville["town"] = url.text
+	hashville["email"] = get_townhall_email("http://annuaire-des-mairies.com" + url['href'][1..-1]), final.push(hashville)
+	}
+
+	puts final
+
+end
+
+def perform
+	get_all_the_emails("http://annuaire-des-mairies.com/val-d-oise.html")
+end
+
+perform
+
+# essayer sleeper
